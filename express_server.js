d444db3a6a5a8d7d99641b7ee987a42aa0327d06
muraahm <ahmedmuraahm121@gmail.com>
2019-08-05 21:23:57 +0000

    updated with urls new

--- express_server.js
+++ express_server.js
@@ -3,6 +3,10 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
